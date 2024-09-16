f07dd03042291a9fd00f7d18a5b35924febcb812
Jacob hart <mayham294@gmail.com>
2024-09-15 23:24:15 -0400

    Print Statements And Variables

---
+++ src/PrintStatements.java
@@ -0,0 +1,19 @@
+
+public class PrintStatements {
+
+	public static void main(String[] arges) {
+		
+		System.out.println("Bro Im a pizza!\n"); // For a space between the 2 lines.
+		System.out.println("Bro Im a chair!"); // For Single Lines.
+		System.out.println("\"Bro Im a table!\""); // For text with "" around it.
+		System.out.println("\\Bro Im a cat!"); // For Text With A \ in it.
+		// short cut for for print statement
+		System.out.println("sysout + Ctrl Space");
+		
+		// Edit Fine/Replace to change a mass amount of same text
+		
+		System.out.println("Ctrl + to zoom in and Ctrl - to zoom out.");
+		
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,38 @@
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		
+		long z = 1234567891; // Can store 10 numbers/letters
+		byte w = 127; // Max Data 127
+		int x; //declaration
+		int y = 321; //initialization
+		float h = 3.14f; // can have decimals.
+		double j = 3.14159; // a float with more data and without f at the end of it.
+		boolean g = false; // Will return True or False - This will return False, and can only be True Or False.
+		boolean f = true; // Will return True or False - This will return True, and can only be True Or False.
+		char symbol = '$'; // Only 1 letter Or Number Or Symbol.
+		String userName = "Fakemayham084"; // Can be any text or number or symbol.
+		
+		x = 123; //assignment
+		
+		// Print Statements
+		System.out.println("X  = " + x);
+		System.out.println("Y = " + y);
+		System.out.println("W = " + w);
+		System.out.println("Z = " + z);
+		System.out.println("H = " + h);
+		System.out.println("J = " + j);
+		System.out.println("G = " + g);
+		System.out.println("F = " + f);
+		System.out.println("Symbol = " + symbol);
+		System.out.println("Username = " + userName);
+		// Print Statements
+		
+		
+		
+		
+		
+	}
+
+}
